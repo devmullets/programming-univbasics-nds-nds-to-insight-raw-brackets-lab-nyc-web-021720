2c1538f94f78a335f29d3650b03621336c9efa5d
Ryan Vaznis <ryan.vaznis@gmail.com>
2020-02-07 00:33:40 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,23 +2,17 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
-  # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
-  #
-  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-
-  # imdb = nds  #import the database
+ 
   row_index = 0 #start with the 1st director in the array
+ 
   while row_index < nds.count do 
     movie_gross = 0 
-    director_name = nds[row_index][:name]
-    # directors_gross = { director_name => 0 }
     directors_gross = {}
     gross_index = 0  
     
     
       while gross_index < nds[row_index][:movies].length do
+        director_name = nds[row_index][:name]
         movie_gross += nds[row_index][:movies][gross_index][:worldwide_gross]
         directors_gross[director_name] = movie_gross
         gross_index += 1
