127e59d3079865f337da85ff35dbc60ba3969890
Ryan Vaznis <ryan.vaznis@gmail.com>
2020-02-07 00:39:59 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,24 +4,18 @@ require 'directors_database'
 def directors_totals(nds)
  
   row_index = 0 #start with the 1st director in the array
-  directors_gross = {}
+  directors_gross = {} # NOTE TO SELF, DO NOT PUT INSIDE OF LOOP LMAO
   while row_index < nds.count do 
     movie_gross = 0 
-    
     gross_index = 0  
-    
       while gross_index < nds[row_index][:movies].length do
         director_name = nds[row_index][:name]
         movie_gross += nds[row_index][:movies][gross_index][:worldwide_gross]
         directors_gross[director_name] = movie_gross
         gross_index += 1
       end 
-    puts "#{director_name} has grossed $#{movie_gross}"
-    # puts "This is what I have logged #{result[director_name]}"
     row_index += 1 
-    
-   end
+  end
   
-  # Be sure to return the result at the end!
   directors_gross
 end
